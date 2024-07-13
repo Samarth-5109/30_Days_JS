0bc257d681926c7a8bd5443f2b6f2ee3f6121999
Samarth Bhardwaj <samarthb5109@gmail.com>
2024-07-13 21:10:26 +0530

    day1 tasks

---
+++ day1.js
@@ -0,0 +1,43 @@
+//activity 1
+// task1
+var number = 43;
+console.log(number);
+
+// task2
+let string = "dell";
+console.log(string);
+
+//activity 2
+//task3
+const bollean = true;
+console.log(bollean);
+
+//activity 3
+//task4
+const age = 25;
+const name = "Samarth";
+const fact = true;
+const obj = {
+  name: "samarth",
+  age: 25,
+};
+const array = ["apple", "banana", "orange"];
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof fact);
+console.log(typeof obj);
+console.log(typeof array);
+
+//activity 4
+//task5
+let score = 25;
+console.log("initital value: " + score);
+score = 50;
+console.log("new value: " + score);
+
+//activity 5
+//task6
+const city = "delhi";
+city = "noida"; //TypeError: Assignment to constant variable.
+//console.log(city)
